65c952cd719eb0794c7a7003bc1281af778b29d5
bharti3bk <bharti3bk@gmail.com>
2019-09-04 10:59:45 -0700

    Added Routers

--- api/index.js
+++
@@ -1,5 +0,0 @@
-const server = require('../api/server.js')
-const port = 5000;
-server.listen(port , () => {
-    console.log(`\n** server is listening on port ${port} **\n`)
-})
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const server = express();
+const server = express();  
+const supllier = require('../routers/supllier') 
 
+//golbal middleware
+server.use(express.json());  
+server.use("/supllier" , supllier)
+//server and route handlers
 server.get('/' , (req , res) => {
     res.status(200).json({api: 'up'})
-}) 
+})  
 
 // export default server 
 module.exports = server;
